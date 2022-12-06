68c434a3d83028476d9d671451b8a778ff06dbdb
natanimabesha <natanimabesha@www.zhewutech.com>
2022-12-06 14:27:55 +0300

    Insert all Queries

--- data.sql
+++ data.sql
@@ -8,3 +8,28 @@ INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) value
 -- Her name is Devimon. She was born on May 12th, 2017, and currently weighs 11kg. She is neutered and she has tried to escape 5 times.
 INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Devimon','2017-May-12',11,true,5);
 
+-- Add a column species of type string to your animals table. Modify your schema.sql file.
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(40);
+
+--  His name is Charmander. He was born on Feb 8th, 2020, and currently weighs -11kg. He is not neutered and he has never tried to escape.
+INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Charmander','2020-Feb-8',11,false,0);
+
+-- Her name is Plantmon. She was born on Nov 15th, 2021, and currently weighs -5.7kg. She is neutered and she has tried to escape 2 times.
+INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Plantmon','2021-Nov-15',5.7,true,2);
+
+-- His name is Squirtle. He was born on Apr 2nd, 1993, and currently weighs -12.13kg. He was not neutered and he has tried to escape 3 times.
+INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Squirtle','1993-Apr-2',12.13,false,3);
+
+-- His name is Angemon. He was born on Jun 12th, 2005, and currently weighs -45kg. He is neutered and he has tried to escape once.
+INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Angemon','2005-Jun-12',45,true,1);
+
+-- His name is Boarmon. He was born on Jun 7th, 2005, and currently weighs 20.4kg. He is neutered and he has tried to escape 7 times.
+INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Boarmon','2005-Jun-7',20.4,true,7);
+
+-- Her name is Blossom. She was born on Oct 13th, 1998, and currently weighs 17kg. She is neutered and she has tried to escape 3 times.
+INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Blossom','1998-Oct-13',17,true,3);
+
+-- His name is Ditto. He was born on May 14th, 2022, and currently weighs 22kg. He is neutered and he has tried to escape 4 times.
+INSERT into animals(name,date_of_birth,weight_kg,neutered,escape_attempts) values('Ditto','2022-May-14',22,true,4);
+
